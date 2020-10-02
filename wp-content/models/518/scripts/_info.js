81b36226ee52acfc268c52460a71c301d2c91ec6
Greg <hello@gregmiller.io>
2020-10-02 15:01:33 -0400

    Remove unused files from versioning

---
+++ wp-content/models/518/scripts/_info.js
@@ -0,0 +1,11 @@
+﻿
+
+var data =
+{
+    "Code": "soss",
+    "Description": "Invinsible Hinge",
+    "ModelPath": "/assets/models/",
+    "Model": "hinge.glb",
+    "Skybox": "./assets/env/CathedralEnvHDR.dds",
+    "Background": "assets/environment/background.jpg",
+}
