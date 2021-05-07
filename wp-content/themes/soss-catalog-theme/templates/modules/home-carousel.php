1270d0dc794def66e90505f0b7ec4844f7b3f575
Greg Miller <hello@gregmiller.io>
2021-05-07 17:34:10 -0400

    Add file that was ignored by Git

---
+++ wp-content/themes/soss-catalog-theme/templates/modules/home-carousel.php
@@ -0,0 +1,152 @@
+<?php
+?>
+
+  <div class="row featured-content-section">
+
+    <div class="row-content">
+      <div class="section-container clearfix">
+        <div class="tab-nav js-tab-nav slick-initialized slick-slider">
+
+
+          <div aria-live="polite" class="slick-list draggable">
+            <div class="slick-track" role="listbox"
+                 style="opacity: 1; width: 1108px; transform: translate3d(0px, 0px, 0px);">
+              <div class="tab-control js-tab-control slick-slide slick-active" data-slick-index="0" aria-hidden="false"
+                   tabindex="-1" role="option" style="width: 277px;">
+                <h3 class="tab-control-heading">
+                  <span class="hover-control js-hover-control">&nbsp;</span>
+                  News
+                </h3>
+              </div>
+              <div class="tab-control js-tab-control slick-slide slick-active" data-slick-index="1" aria-hidden="false"
+                   tabindex="-1" role="option" style="width: 277px;">
+                <h3 class="tab-control-heading">
+                  <span class="hover-control js-hover-control">&nbsp;</span>
+                  Projects
+                </h3>
+              </div>
+              <div class="tab-control js-tab-control slick-slide slick-active" data-slick-index="2" aria-hidden="false"
+                   tabindex="-1" role="option" style="width: 277px;">
+                <h3 class="tab-control-heading">
+                  <span class="hover-control js-hover-control">&nbsp;</span>
+                  Blog
+                </h3>
+              </div>
+              <div class="tab-control js-tab-control slick-slide slick-active slick-current" data-slick-index="3"
+                   aria-hidden="false" tabindex="-1" role="option" style="width: 277px;">
+                <h3 class="tab-control-heading">
+                  <span class="hover-control js-hover-control">&nbsp;</span>
+                  Videos
+                </h3>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+        <div class="tabs-block gallery-tab js-panel-slider clearfix slick-initialized slick-slider">
+
+
+          <div aria-live="polite" class="slick-list draggable">
+            <div class="slick-track" role="listbox" style="opacity: 1; width: 4424px;">
+              <div class="tab-content slick-slide" data-slick-index="0" aria-hidden="true" tabindex="-1" role="option"
+                   style="width: 1106px; position: relative; left: 0px; top: 0px; z-index: 998; opacity: 0; transition: opacity 500ms ease 0s;">
+                <div class="block-right">
+                  <a href="/professionals/architects-and-designers/best-of-lacantina" class="content-link"
+                     aria-label="View News Content" tabindex="-1">
+                    <img
+                      src="https://www.lacantinadoors.com/images/uploads/homepage/Website_News_ArchComp2020_HomepageImage_570x386_v2.jpg"
+                      alt="">
+                  </a>
+                </div>
+
+                <div class="block-left">
+                  <h2>BEST OF LaCANTINA 2020 WINNERS ANNOUNCED</h2>
+
+                  <p>We are excited to share the standout designs from this year’s competition, projects that truly
+                    epitomize “The Best of LaCantina”.</p>
+
+                  <p><a href="/professionals/architects-and-designers/best-of-lacantina" class="button blue"
+                        tabindex="-1">Read More</a></p>
+                </div>
+              </div>
+              <div class="tab-content slick-slide" data-slick-index="1" aria-hidden="true" tabindex="-1" role="option"
+                   style="width: 1106px; position: relative; left: -1106px; top: 0px; z-index: 998; opacity: 0; transition: opacity 500ms ease 0s;">
+                <div class="block-right">
+                  <a href="/learn/project/landsberg-house" class="content-link" aria-label="View Projects Content"
+                     tabindex="-1">
+                    <img
+                      src="https://www.lacantinadoors.com/images/uploads/homepage/Website_FeatProj_Landsberg_HomepageImage_570x386.jpg"
+                      alt="">
+                  </a>
+                </div>
+
+                <div class="block-left">
+                  <h2>Landsberg House</h2>
+
+                  <p>This mid-century home was the house of architect William Landsberg, who gained fame for his
+                    designs. The renovation of this home incorporated contemporary Japanese lifestyle elements with
+                    modern designs, natural elements and LaCantina Doors that complement the surrounding nature.</p>
+
+                  <p><a href="/learn/project/landsberg-house" class="button blue" tabindex="-1">View Project</a></p>
+                </div>
+              </div>
+              <div class="tab-content slick-slide" data-slick-index="2" aria-hidden="true" tabindex="-1" role="option"
+                   style="width: 1106px; position: relative; left: -2212px; top: 0px; z-index: 998; opacity: 0; transition: opacity 500ms ease 0s;">
+                <div class="block-right">
+                  <a href="blog/entry/accordion-glass-patio-doors" class="content-link" aria-label="View Blog Content"
+                     tabindex="-1">
+                    <img
+                      src="https://www.lacantinadoors.com/images/uploads/homepage/Website_Blog_Rima_HomepageImage_570x386.jpg"
+                      alt="">
+                  </a>
+                </div>
+
+                <div class="block-left">
+                  <h2>Why Accordion Glass Patio Doors Should Be Your Next Home Improvement Project.</h2>
+
+                  <p>Home renovations are a great way to update an existing space while adding value to your property.
+                    One easy way to transform a cramped, closed-off space is by replacing a wall with accordion glass
+                    doors.</p>
+
+                  <p><a href="blog/entry/accordion-glass-patio-doors" class="button blue" tabindex="-1">Read More</a>
+                  </p>
+                </div>
+              </div>
+              <div class="tab-content slick-slide slick-current slick-active" data-slick-index="3" aria-hidden="false"
+                   tabindex="-1" role="option"
+                   style="width: 1106px; position: relative; left: -3318px; top: 0px; z-index: 999; opacity: 1;">
+                <div class="block-right">
+
+                  <a href="//view.vzaar.com/17947562/player?GAOn=true" class="content-link js-home-video-modal-link"
+                     aria-label="Play Video Content" tabindex="0">
+                    <div class="play-btn">&nbsp;</div>
+
+                    <img
+                      src="https://www.lacantinadoors.com/images/uploads/homepage/Website_FeatVideo_InstallVideos_HomepageImage_570x386.jpg"
+                      alt="">
+                  </a>
+                </div>
+
+                <div class="block-left">
+                  <h2>LaCantina&nbsp;Launches<br>
+                    Installation Video Series</h2>
+
+                  <p>Follow along as TV host and Licensed Contractor, Josh Temple, and General Manager of LaCantina, Lee
+                    Maughan guide you through a comprehensive video series on how to install LaCantina's Folding,<br>
+                    Multi Slide and Screen Systems.&nbsp;</p>
+
+                  <p><a href="/gallery/video-gallery/videos-installation" class="button blue" tabindex="0">View All
+                      Videos</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+<?php
