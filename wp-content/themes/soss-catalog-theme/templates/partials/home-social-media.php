17928a74c06770ab9da1d54e79e074526a2a9a2f
Greg Miller <hello@gregmiller.io>
2021-05-13 21:32:31 -0400

    Build out Social properties template for refactored homepage * Tweak button styles * Refactor frontpage styles

--- wp-content/themes/soss-catalog-theme/templates/partials/home-social-media.php
+++ wp-content/themes/soss-catalog-theme/templates/partials/home-social-media.php
@@ -6,13 +6,75 @@
    * @author gmiller
    * @date: 5/13/2021
    */
-  $filename = '';
-  $filename = basename($_SERVER["SCRIPT_FILENAME"], '.php');
-  echo $filename;
+
+  $select_property = get_sub_field('select_from_list_of_properties');
+  $follow_label = get_sub_field('add_follow_words');
+  $i_prop = '@SOSSD';
+  $f_prop = 'SOSS Door Hardware';
+  $l_prop = 'SOSS Door Hardware';
+  $t_prop = '@SOSSDoors';
+
+?>
+<div class="break-out">
+  <div class="container">
+  <div class="row">
+    <div class="col d-flex justify-content-center my-3">
+      <div class="push d-flex  align-items-center">
+
+<?php
+
+  switch ( $select_property ) {
+    case 'insta':
+      ?>
+
+        <i class="fa fa-instagram fa-2x"></i>
+        <span class="property "><?= $i_prop; ?></span>
+        <a href="" class="btn btn-primary cta-brand" title="" target="_blank"><?=  $follow_label; ?></a>
+
+      <?php
+      break;
+    case 'face':
+      ?>
+
+        <i class="fa fa-facebook-square fa-2x"></i>
+        <span class="$font-size-base"><?= $f_prop; ?></span>
+        <a href="https://www.facebook.com/SOSS-Door-Hardware-254546971278920/"
+           class="btn btn-primary cta-brand" title="" target="_blank" ><?= $follow_label; ?></a>
+
+      <?php
+      break;
+    case 'link':
+      ?>
+      <i class="fa fa-linkedin-square fa-2x"></i>
+      <span class="$font-size-base"><?= $l_prop; ?></span>
+      <a href="https://www.linkedin.com/company/soss-door-hardware/"
+         class="btn btn-primary cta-brand" title="" target="_blank"><?= $follow_label; ?></a>
+
+
+      <?php
+      break;
+    case 'tweet':
+      ?>
+      <i class="fa fa-twitter-square fa-2x"></i>
+      <span class="$font-size-base"><?= $t_prop; ?></span>
+      <a href="https://twitter.com/SOSSDoors"
+         class="btn btn-primary cta-brand" title="" target="_blank"><?= $follow_label; ?></a>
+
+
+      <?php
+      break;
+  }
+?>
+      </div>
+    </div>
+    </div>
+  </div>
+</div>
+
+<?php
 ?>
 
-<?php the_sub_field( 'select_from_list_of_properties' ); ?>
-<?php the_sub_field( 'add_follow_words' ); ?>
+
 
 <?php // end Row Layout
 
