9e34ae2937f10bb0934008872639c8cad44bccc1
Greg Miller <hello@gregmiller.io>
2021-05-14 16:02:19 -0400

    Refactor to build home blocks template

--- wp-content/themes/soss-catalog-theme/templates/partials/home-block-component.php
+++ wp-content/themes/soss-catalog-theme/templates/partials/home-block-component.php
@@ -1,4 +1,5 @@
 <?php
+
   /**
    * Feature Block Component
    * @description: User uses repeater to create up to three boxes. They will add an image background, add foreground
@@ -6,28 +7,43 @@
    * @author gmiller
    * @date: 5/13/2021
    */
-  $filename = '';
-  $filename = basename($_SERVER["SCRIPT_FILENAME"], '.php');
-  echo $filename;
+
+  declare( strict_types=1 );
+
+  $heading = get_sub_field('block_row_heading');
+
 ?>
 
 <?php the_sub_field( 'block_row_heading' ); ?>
 <?php if ( have_rows( 'block_builder' ) ) : ?>
   <?php while ( have_rows( 'block_builder' ) ) : the_row(); ?>
-    <?php $background_image = get_sub_field( 'background_image' ); ?>
-    <?php $size = 'full'; ?>
-    <?php if ( $background_image ) : ?>
-      <?php echo wp_get_attachment_image( $background_image, $size ); ?>
+    <?php
+    $image_id = get_sub_field('background_image');
+    $size = 'full';
+    $bg_image = wp_get_attachment_image($image_id, $size, false, $attr = ['class' => 'img-fluid  align-self-center']);
+
+// https://codepen.io/shrinkray/pen/LgNKXY?editors=1100
+     if ( $bg_image ) : ?>
+
+       <?php echo  $bg_image ?>
+
     <?php endif; ?>
-    <?php if ( have_rows( 'button' ) ) : ?>
-      <?php while ( have_rows( 'button' ) ) : the_row(); ?>
-        <?php the_sub_field( 'title' ); ?>
-        <?php $link = get_sub_field( 'link' ); ?>
-        <?php if ( $link ) : ?>
-          <a href="<?php echo esc_url( $link); ?>"><?php echo esc_html( $link ); ?></a>
+
+    <?php if ( have_rows( 'button' ) ) :
+
+      while ( have_rows( 'button' ) ) : the_row();
+
+        $box_title = get_sub_field('title');
+        $box_link = get_sub_field('link');
+
+        if ( $box_link ) : ?>
+          <a href="<?php echo esc_url( $box_link); ?>"><?= $box_title ?></a>
         <?php endif; ?>
+
       <?php endwhile; ?>
+
     <?php endif; ?>
+
   <?php endwhile; ?>
 <?php else :
  // no rows found ?>
