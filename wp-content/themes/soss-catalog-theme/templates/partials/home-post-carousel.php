e4d52cd4d133edbb6ba4818f927ba85ff37ba005
Greg Miller <hello@gregmiller.io>
2021-07-07 20:08:09 -0400

    Refactor post carousel * Set left column to flex as column * Rename .card-body to .card-body-center to avoid default styling from Bootstrap * If no thumbnail image, display a centered CTA * Apply Bootstrap utility classes for mobile

--- wp-content/themes/soss-catalog-theme/templates/partials/home-post-carousel.php
+++ wp-content/themes/soss-catalog-theme/templates/partials/home-post-carousel.php
@@ -16,7 +16,7 @@ $args = [
   <div class="homepage-post-carousel ">
     <div class="row">
 
-      <div class="col-lg-10 offset-lg-1 col-md-12">
+      <div class="col-lg-6 offset-lg-3 col-md-10 offset-md-1 col-sm-12">
        <ul class="carousel-panel ">
     <?php
       if ( $post_query->have_posts() ) :
@@ -38,18 +38,34 @@ $args = [
 
               ?>
 
-              <div class="col-md-6 order-md-1 order-sm-2 order-xs-2 align-content-md-center">
-                <div class="card-body ">
-                  <?php the_title('<h3 class="card-title">', '</h3>'); ?>
-                  <?php the_excerpt(); ?>
-                  <a href="<?php echo esc_url(get_the_permalink()); ?>" class="button cm-tab-brand">
-                    <?php esc_html_e('View More', 'sage'); ?>
-                  </a>
+              <div class="col-md-6 d-flex flex-column justify-content-center order-md-1 order-sm-2 order-xs-2">
+                <div class="card-body-center">
+
+                    <?php the_title('<h3 class="card-title">', '</h3>'); ?>
+                    <?php the_excerpt(); ?>
+                    <a href="<?php echo esc_url(get_the_permalink()); ?>" class="button cm-tab-brand">
+                      <?php esc_html_e('View More', 'sage'); ?>
+                    </a>
+
                 </div>
               </div>
 
               <div class="col-md-6 order-md-2 order-sm-1 order-xs-1 d-flex align-items-center">
                 <?php the_post_thumbnail('feature_post', ['class' => 'img-fluid lazyloaded']); ?>
+              </div>
+
+          <?php else : // no thumbnail image -- Not tested ?>
+
+              <div class="col-md-6 offset-md-3 col-sm-8 offset-sm-2 d-flex flex-column justify-content-center">
+                <div class="card-body-center">
+
+                  <?php the_title('<h3 class="card-title">', '</h3>'); ?>
+                  <?php the_excerpt(); ?>
+                  <a href="<?php echo esc_url(get_the_permalink()); ?>" class="button cm-tab-brand">
+                    <?php esc_html_e('View More', 'sage'); ?>
+                  </a>
+
+                </div>
               </div>
 
             <?php endif; ?>
